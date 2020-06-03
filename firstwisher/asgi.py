edb9a4faf1aa4c8a53a531021970d7cd3913ad7c
Harsh Buddhdev <ht50159@gmail.com>
2020-06-04 04:49:50 +0530

    h

--- firstwisher/asgi.py
+++ firstwisher/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for wisher project.
+ASGI config for firstwisher project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wisher.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstwisher.settings')
 
 application = get_asgi_application()
