2622451fc759890183b9d933643b0ada787f191e
Harsh Buddhdev <ht50159@gmail.com>
2020-06-04 01:37:27 +0530

    yes?

--- wisher/settings.py
+++ wisher/settings.py
@@ -127,5 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'wisherb/static/wisherb'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'wisherb/static/wisherb')
 
 django_heroku.settings(locals())
