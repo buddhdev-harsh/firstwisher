1fd8ed07d58bccc39894dc47f42f86a9ca5592b1
Harsh Buddhdev <ht50159@gmail.com>
2020-06-04 01:32:17 +0530

    yes?

--- wisher/settings.py
+++ wisher/settings.py
@@ -123,10 +123,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-   
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'wisherb/static/wisherb'),
+)
 
 django_heroku.settings(locals())
--- wisher/urls.py
+++ wisher/urls.py
@@ -23,6 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('wisherb.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
 
 urlpatterns += staticfiles_urlpatterns()
